9d29f5d000fd44d7ecb58cd9c4eea94e10091571
Joy Brar <joybrar2001@gmail.com>
2024-06-23 18:16:50 +0530

    Communities fix

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -189,12 +189,12 @@ export async function fetchThreadById(threadId: string) {
   }
 }
 
-export async function addCommentToThread(
+export async function addCommentToThread({threadId, commentText, userId, path}: {
   threadId: string,
   commentText: string,
   userId: string,
   path: string
-) {
+}) {
   connectToDB();
 
   try {
